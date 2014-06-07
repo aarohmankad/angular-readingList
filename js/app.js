2c12dd2f3e6a0fe586428266ce80b01da681a3fb
Aaroh <aaroh@ubuntu.(none)>
2014-06-07 10:12:48 -0700

    you can now mark books as read

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@
   		review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis ipsum inventore fugiat. Perspiciatis, enim, nesciunt, modi officiis illum corrupti temporibus incidunt voluptatibus eius magnam commodi tempora! Dolorem, dicta necessitatibus vero?",
   		rating: 4,
   		genre: "Fiction",
+      currently: false,
   	},
   	{
   		title: "Siddhartha",
@@ -17,6 +18,7 @@
   		review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, fugit, perspiciatis blanditiis beatae quod praesentium quasi ratione obcaecati dolore neque eligendi sed necessitatibus dolorum quaerat provident impedit asperiores explicabo hic!",
   		rating: 5,
   		genre: "Alternate History",
+      currently: false,
   	}
   	];
   });
@@ -33,7 +35,13 @@
     return{
       replace: true,
       restrict: 'E',
-      templateUrl: '../views/book-info.html'
+      templateUrl: '../views/book-info.html',
+      controller: function ($scope) {
+        $scope.changeStatus = function (book) {
+          book.currently = !book.currently;
+        }
+      },
+      controllerAs: 'bookInfo'
     };
   });
 
